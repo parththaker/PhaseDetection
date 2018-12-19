fb0e663f6009ecf57cc9cc4ae733e316943beb66
parththaker <parneh@gmail.com>
2018-12-18 23:41:24 -0700

    Added Incremental and variance reduction based methods

--- base_method.py
+++ base_method.py
@@ -13,6 +13,8 @@ A, x_star, x_0, psi = support.generate_random_quant(n, m)
 norm_method = function.NormMethods(n, m, niter, A, x_star, x_0, psi)
 taf = function.AmplitudeFlow(n, m, niter, A, x_star, x_0, psi)
 twf = function.WirtingerFlow(n, m, niter, A, x_star, x_0, psi)
+stwf = function.SAGWirtingerFlow(n, m, niter, A, x_star, x_0, psi)
+staf = function.SAGAmplitudeFlow(n, m, niter, A, x_star, x_0, psi)
 kz = function.Kaczmarc(n, m, niter, A, x_star, x_0, psi)
 
 l1_arr = norm_method.run_iter(error_bound, order = 1)
@@ -22,21 +24,42 @@ l2_inc_arr = norm_method.run_iter(error_bound, order = 2, inc=1)
 linf_arr = norm_method.run_iter(error_bound, order = np.inf)
 af_arr = taf.run_iter(active=0)
 taf_arr = taf.run_iter(active=1)
+taf_inc_arr = taf.run_iter(active=1, inc=1)
 wf_arr = twf.run_iter(active=0)
 twf_arr = twf.run_iter(active=1)
+twf_inc_arr = twf.run_iter(active=1, inc=1)
+sag_twf_arr = stwf.run_iter(active=1)
+sag_wf_arr = stwf.run_iter(active=0)
+sag_taf_arr = staf.run_iter(active=1)
+sag_af_arr = staf.run_iter(active=0)
 kz_arr = kz.run_iter()
 
 plt.figure(1)
 plt.semilogy(range(niter), af_arr, label='AF')
-plt.semilogy(range(niter), taf_arr, label='TAF')
 plt.semilogy(range(niter), wf_arr, label='WF')
+
+
+plt.semilogy(range(niter), taf_arr, label='TAF')
 plt.semilogy(range(niter), twf_arr, label='TWF')
+
+plt.semilogy(range(niter), taf_inc_arr, label='TAF Inc')
+plt.semilogy(range(niter), twf_inc_arr, label='TWF Inc')
+
+plt.semilogy(range(niter), sag_wf_arr, label='SAG WF')
+plt.semilogy(range(niter), sag_af_arr, label='SAG AF')
+
+plt.semilogy(range(niter), sag_twf_arr, label='SAG TWF')
+plt.semilogy(range(niter), sag_taf_arr, label='SAG TAF')
+
 plt.semilogy(range(niter), kz_arr, label='Kaczmarc')
+
 plt.semilogy(range(niter), l1_arr, label='Prox. l1')
 plt.semilogy(range(niter), l2_arr, label='Prox. l2')
+plt.semilogy(range(niter), linf_arr, label='Prox. linf')
+
 plt.semilogy(range(niter), l1_inc_arr, label='Prox. Inc. l1')
 plt.semilogy(range(niter), l2_inc_arr, label='Prox. Inc. l2')
-plt.semilogy(range(niter), linf_arr, label='Prox. linf')
+
 plt.xlabel('Iterate no.')
 plt.ylabel(r'$\sum_{i=1}^m||a_i^Tx - \psi_i||$')
 plt.title('Error comparision between TAF, Prox. Linear and Augmeneted lagrangian approach')
--- function.py
+++ function.py
@@ -70,7 +70,7 @@ class AmplitudeFlow:
             s += (np.dot(self.A[i], x) - self.psi[i] * (np.dot(self.A[i], x) / np.abs(np.dot(self.A[i], x)))) * self.A[i]
         return x - (1. * self.alpha / self.m) * s
 
-    def run_iter(self, active=1):
+    def run_iter(self, active=1, inc=0):
         self.active = active
         trial = 0
         err_arr = []
@@ -80,6 +80,9 @@ class AmplitudeFlow:
             active_set = range(self.m)
             if self.active:
                 active_set = self.get_active_set(x)
+            if inc:
+                index = np.random.choice(active_set)
+                active_set = [index]
             x = self.update_x(active_set, x)
             error = support.check_error(self.A, x, self.xstar, self.m)
             err_arr.append(error)
@@ -120,7 +123,7 @@ class WirtingerFlow:
             s += ( ( self.psi[i]**2 - np.dot(self.A[i], x)**2 ) / np.dot(self.A[i], x) ) * self.A[i]
         return x + (2*self.mu/self.m) * s
 
-    def run_iter(self, active=1):
+    def run_iter(self, active=1, inc=0):
         self.active = active
         trial = 0
         err_arr = []
@@ -130,6 +133,9 @@ class WirtingerFlow:
             active_set = range(self.m)
             if self.active:
                 active_set = self.get_active_set(x)
+            if inc:
+                index = np.random.choice(active_set)
+                active_set = [index]
             x = self.update_x(active_set, x)
             error = support.check_error(self.A, x, self.xstar, self.m)
             err_arr.append(error)
@@ -137,6 +143,7 @@ class WirtingerFlow:
 
 
 class Kaczmarc:
+
     def __init__(self, n, m, niter, A, xstar, x0, psi):
         self.niter = niter
         self.n = n
@@ -159,6 +166,120 @@ class Kaczmarc:
         while trial < self.niter:
             trial += 1
             x = self.update_x(x)
+            error = support.check_error(self.A, x, self.xstar, self.m)
+            err_arr.append(error)
+        return err_arr
+
+
+class SAGWirtingerFlow:
+
+    def __init__(self, n, m, niter, A, xstar, x0, psi):
+        self.niter = niter
+        self.n = n
+        self.m = m
+        self.alpha_h = 5.0
+        self.alpha_y = 3.0
+        self.alpha_lb = 0.3
+        self.alpha_ub = 5.0
+        self.mu = 0.3
+        self.A = A
+        self.xstar = xstar
+        self.x0 = x0
+        self.active = 1
+        self.psi = psi
+        self.xold = x0
+        self.L = np.linalg.norm(self.A)
+
+    def get_active_set(self, x):
+        angles = [math.sqrt(self.n)*np.abs(np.dot(self.A[i], x))/np.linalg.norm(self.A[i])/np.linalg.norm(x) for i in range(self.m)]
+        violations = [np.abs(self.psi[i]**2 - np.dot(self.A[i], x)**2) for i in range(self.m)]
+
+        h = []
+        for i in range(self.m):
+            if self.alpha_lb <= angles[i] <= self.alpha_ub and violations[i] <= self.alpha_h*(sum(violations)/self.m)*angles[i]:
+                h.append(i)
+        return h
+
+    def compute_old_grad(self, active_set):
+        s = 0.0
+        for i in active_set:
+            s += ((self.psi[i] ** 2 - np.dot(self.A[i], self.xold) ** 2) / np.dot(self.A[i], self.xold)) * self.A[i]
+        return (1/ self.m) * s
+
+    def update_x(self, active_set, x):
+        i = np.random.choice(active_set)
+        s_new = ( ( self.psi[i]**2 - np.dot(self.A[i], x)**2 ) / np.dot(self.A[i], x) ) * self.A[i]
+        s_old = ( ( self.psi[i]**2 - np.dot(self.A[i], self.xold)**2 ) / np.dot(self.A[i], self.xold) ) * self.A[i]
+        return x + (2*self.mu*0.3) * (s_new - s_old + self.compute_old_grad(active_set))
+
+    def run_iter(self, active=1):
+        self.active = active
+        trial = 0
+        err_arr = []
+        x = self.x0
+        self.xold = self.x0
+        while trial < self.niter:
+            trial += 1
+            active_set = range(self.m)
+            if self.active:
+                active_set = self.get_active_set(x)
+            xold = x
+            x = self.update_x(active_set, x)
+            if trial>1:
+                self.xold = xold
+            error = support.check_error(self.A, x, self.xstar, self.m)
+            err_arr.append(error)
+        return err_arr
+
+
+class SAGAmplitudeFlow:
+
+    def __init__(self, n, m, niter, A, xstar, x0, psi):
+        self.niter = niter
+        self.n = n
+        self.m = m
+        self.gamma = 0.7
+        self.alpha = 0.6
+        self.A = A
+        self.xstar = xstar
+        self.x0 = x0
+        self.active = 1
+        self.xold = x0
+        self.psi = psi
+        self.L = np.linalg.norm(self.A)
+
+    def get_active_set(self, x):
+        return [i for i in range(self.m) if np.abs(np.dot(self.A[i], x)) > self.psi[i] / (1. + self.gamma)]
+
+    def compute_old_grad(self, active_set):
+        s = 0.0
+        for i in active_set:
+            s += (np.dot(self.A[i], self.xold) - self.psi[i] * (np.dot(self.A[i], self.xold) / np.abs(np.dot(self.A[i], self.xold)))) * self.A[i]
+        return (1./self.m)*s
+
+    def update_x(self, active_set, x):
+        i = np.random.choice(active_set)
+        s_new = (np.dot(self.A[i], self.xold) - self.psi[i] * (np.dot(self.A[i], self.xold) / np.abs(np.dot(self.A[i], self.xold)))) * self.A[i]
+        s_old = (np.dot(self.A[i], x) - self.psi[i] * (np.dot(self.A[i], x) / np.abs(np.dot(self.A[i], x)))) * self.A[i]
+        return x - (1. * self.alpha) * ( self.compute_old_grad(active_set) + s_new - s_old)
+
+    def run_iter(self, active=1, inc=0):
+        self.active = active
+        trial = 0
+        err_arr = []
+        x = self.x0
+        self.xold = self.x0
+        while trial < self.niter:
+            trial += 1
+            active_set = range(self.m)
+            if self.active:
+                active_set = self.get_active_set(x)
+            if inc:
+                index = np.random.choice(active_set)
+                active_set = [index]
+            xold = x
+            x = self.update_x(active_set, x)
+            self.xold = xold
             error = support.check_error(self.A, x, self.xstar, self.m)
             err_arr.append(error)
         return err_arr
